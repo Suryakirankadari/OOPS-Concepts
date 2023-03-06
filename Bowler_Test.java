9f49151014369acfa35c567512b8e152ee04e283
Suryakirankadari <93572415+Suryakirankadari@users.noreply.github.com>
2023-03-06 15:02:14 +0530

    Add files via upload

---
+++ Bowler_Test.java
@@ -0,0 +1,14 @@
+package assignments;
+
+public class Bowler_Test {
+
+	  public static void main(String[] args) {
+	    Bowler b1= new Bowler("Sachin",10,5,750,463);
+	    b1.computeBowlingAverage();
+	    b1.computeStrikeRate();
+	    b1.showStatics();
+	  }
+
+	}
+
+
